f5daed8ad568d83be71fb59ecef2d18574d7174a
ekoneko <9@ikaze.me>
2017-04-23 18:49:52 +0800

    feat: create request item file

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const createProject = require('./lib/createProject')
+const createItem = require('./lib/createItem')
 
 module.exports = {
   createProject(projectName) {
     createProject(projectName)
   },
-  createItem() {
-    //
+  createItem(itemName) {
+    createItem(itemName)
   },
   request() {
     //
